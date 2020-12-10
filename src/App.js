7bad70168120b495005cfb4fb1a52db74dd24d72
vovababich <vova_babich@list.ru>
2020-12-10 19:19:09 +0200

    saved

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { Component } from 'react';
+import Section from './component/Section';
+import Feedback from './component/Feedback';
+import Statistics from './component/Statistics';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  feedbackOptions = ['good', 'neutral', 'bad'];
+
+  hendleIncrement = feedback => {
+    this.setState(prevState => {
+      this.setState({ [feedback]: prevState[feedback] + 1 });
+    });
+  };
+
+  render() {
+    return (
+      <Section>
+        <Feedback
+          title="Please leave feedback"
+          options={this.feedbackOptions}
+          onLeaveFeedback={this.hendleIncrement}
+        />
+
+        <Statistics title="Statistics" state={this.state} />
+      </Section>
+    );
+  }
+}
+export default App;
