fbf444d3bf52d9d7fb283c542d80a97fdef382f2
vovababich <vova_babich@list.ru>
2020-12-23 01:18:06 +0200

    corrected the mentor"s comments

--- src/App.js
+++ src/App.js
@@ -16,7 +16,21 @@ class App extends Component {
     this.setState(prevState => ({ [feedback]: prevState[feedback] + 1 }));
   };
 
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    return Math.round((good / total) * 100);
+  };
+
   render() {
+    const total = this.countTotalFeedback();
+    const positiveFeedback = this.countPositiveFeedbackPercentage();
+
     return (
       <Section>
         <Feedback
@@ -25,7 +39,12 @@ class App extends Component {
           onLeaveFeedback={this.hendleIncrement}
         />
 
-        <Statistics title="Statistics" state={this.state} />
+        <Statistics
+          title="Statistics"
+          state={this.state}
+          total={total}
+          positiveFeedback={positiveFeedback}
+        />
       </Section>
     );
   }
